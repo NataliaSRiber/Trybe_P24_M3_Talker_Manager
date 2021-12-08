e338b87bc2040d4f31dcbe5dcfe9c2e29b03382f
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-12-08 14:56:50 -0300

    requisito 1

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const fs = require('fs/promises');
+
+const talkersJson = './talker.json';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  const talkers = await fs.readFile(talkersJson, 'utf-8');
+  response.status(HTTP_OK_STATUS).send(JSON.parse(talkers));
 });
 
 app.listen(PORT, () => {
